18e3379a51ed4bfe8c093e47dcd7bb7478528494
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2024-12-03 10:19:04 -0300

    Day 3

---
+++ day_03/src/lib.rs
@@ -0,0 +1,85 @@
+use std::str::FromStr;
+
+use anyhow::Context;
+use regex_static::{Regex, lazy_regex, once_cell::sync::Lazy};
+
+// TODO: In the future consider using nom instead?
+const INSTRUCTION_REGEX: Lazy<Regex> = lazy_regex!(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)");
+
+pub fn execute_instructions(input: &str) -> i64 {
+    INSTRUCTION_REGEX.captures_iter(input)
+        .map(|c| (
+            c.get(1).unwrap().as_str().parse::<i64>().unwrap(),
+            c.get(2).unwrap().as_str().parse::<i64>().unwrap(),
+        ))
+        .map(|(a, b)| a * b)
+        .sum()
+}
+
+pub fn execute_instructions_conditionally(input: &str) -> i64 {
+    let instructions = Instruction::many_from_str(input).unwrap();
+    let mut enabled = true;
+    instructions.iter()
+        .map(|i| i.execute_conditionally(&mut enabled))
+        .sum()
+}
+
+
+pub enum Instruction {
+    Mul(i64, i64),
+    Do,
+    Dont,
+}
+
+impl Instruction {
+    pub fn value(&self) -> i64 {
+        match self {
+            Instruction::Mul(a, b) => a * b,
+            _ => 0
+        }
+    }
+
+    pub fn execute_conditionally(&self, enabled: &mut bool) -> i64 {
+        // dbg!(*enabled);
+        match self {
+            Instruction::Do => {
+                *enabled = true;
+                0
+            },
+            Instruction::Dont => {
+                *enabled = false;
+                // println!("Disabled!");
+                0
+            },
+            _ => if *enabled { self.value() } else { 0 }
+        }
+    }
+
+    pub fn many_from_str(s: &str) -> Result<Vec<Self>, anyhow::Error> {
+        INSTRUCTION_REGEX
+            .find_iter(s)
+            .map(|m| Self::from_str(m.as_str()))
+            .collect()
+    }
+}
+
+impl FromStr for Instruction {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parse = INSTRUCTION_REGEX
+            .captures(s)
+            .context("Not an instruction")?;
+        Ok(match parse.get(0).unwrap().as_str() {
+            ins if ins.starts_with("mul") => {
+                Self::Mul(
+                    parse.get(1).unwrap().as_str().parse::<i64>().unwrap(),
+                    parse.get(2).unwrap().as_str().parse::<i64>().unwrap(),
+                )
+            },
+            ins if ins.starts_with("don't") => Self::Dont,
+            ins if ins.starts_with("do") => Self::Do,
+            _ => unreachable!(),
+        })
+    }
+}
