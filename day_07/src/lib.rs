f6b3491400d7bf7d02eb5f4a673a205b79fe899f
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2024-12-07 09:16:04 -0300

    Day 7 - Refactors

--- day_07/src/lib.rs
+++ day_07/src/lib.rs
@@ -1,54 +1,48 @@
+use std::ops::{Add, Mul};
 
-pub fn get_calibration_sum_1(input: &str) -> i64 {
+type OperatorFns<'a> = [&'a dyn Fn(u64, u64) -> u64];
+
+pub fn get_calibration_sum_1(input: &str) -> u64 {
+    get_calibration_result(input, &[&Add::add, &Mul::mul])
+}
+
+pub fn get_calibration_sum_2(input: &str) -> u64 {
+    get_calibration_result(input, &[&Add::add, &Mul::mul, &concatenate_nums])
+}
+
+fn get_calibration_result(input: &str, operators: &OperatorFns) -> u64 {
     input.lines()
         .map(|l| {
             let (expected, nums) = l.split_once(':').unwrap();
             let nums = nums.trim()
                 .split_whitespace()
-                .map(|n| n.parse::<i64>().unwrap())
+                .map(|n| n.parse::<u64>().unwrap())
                 .collect::<Vec<_>>();
-            (expected.parse::<i64>().unwrap(), nums)
+            (expected.parse::<u64>().unwrap(), nums)
         })
-        .filter(|(expected, nums)| can_be_solved(*expected, nums))
+        .filter(|(expected, nums)| can_be_solved(*expected, nums, operators))
         .map(|(expected, _)| expected)
         .sum()
 }
 
-fn can_be_solved(expected: i64, nums: &[i64]) -> bool {
+fn can_be_solved(expected: u64, nums: &[u64], operators: &OperatorFns) -> bool {
     let first = *nums.first().unwrap();
-    can_be_solved_recursive(expected, first, &nums[1..])
+    can_be_solved_recursive(expected, first, &nums[1..], operators)
 }
 
-fn can_be_solved_recursive(expected: i64, already_calculated: i64, nums: &[i64]) -> bool {
+fn can_be_solved_recursive(expected: u64, already_calculated: u64, nums: &[u64], operators: &OperatorFns) -> bool {
     let next = match nums.first().cloned() {
         Some(n) => n,
         None => return expected == already_calculated,
     };
 
     let rest = &nums[1..];
-    can_be_solved_recursive(expected, already_calculated + next, rest)
-        || can_be_solved_recursive(expected, already_calculated * next, rest)
-        || can_be_solved_recursive(expected, concatenate_nums(already_calculated, next), rest)
+    operators.iter()
+        .any(|op| can_be_solved_recursive(
+            expected, op(already_calculated, next), rest, operators))
 }
 
-fn can_be_solved_2(expected: i64, nums: &[i64]) -> bool {
-    let first = *nums.first().unwrap();
-    can_be_solved_recursive(expected, first, &nums[1..])
-}
-
-fn can_be_solved_recursive_2(expected: i64, already_calculated: i64, nums: &[i64]) -> bool {
-    let next = match nums.first().cloned() {
-        Some(n) => n,
-        None => return expected == already_calculated,
-    };
-
-    let rest = &nums[1..];
-    can_be_solved_recursive_2(expected, already_calculated + next, rest)
-        || can_be_solved_recursive_2(expected, already_calculated * next, rest)
-        || can_be_solved_recursive_2(expected, concatenate_nums(already_calculated, next), rest)
-}
-
-fn concatenate_nums(lhs: i64, rhs: i64) -> i64 {
+fn concatenate_nums(lhs: u64, rhs: u64) -> u64 {
     let s = format!("{lhs}{rhs}");
     s.parse().unwrap()
 }
