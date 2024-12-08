f7c8f4f0dae6e52bbd55671187eeb1c1f1a8302b
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2024-12-08 09:52:56 -0300

    Day 8 - Part 1

---
+++ day_08/src/lib.rs
@@ -0,0 +1,31 @@
+use std::{collections::{HashMap, HashSet}, str::FromStr};
+use xmas::map2d::{ByteMap, ParseMapError};
+
+
+pub fn calculate_antinodes_locations(input: &str) -> Result<usize, ParseMapError> {
+    let map = ByteMap::from_str(input)?;
+    let mut antenas_by_id = HashMap::new();
+    for (point, tile) in map.iter_with_points().filter(|&(_, t)| t != &b'.' && t != &b'#') {
+        let entry = antenas_by_id.entry(tile).or_insert_with(|| vec![]);
+        entry.push(point);
+    }
+
+    let mut antinodes = HashSet::new();
+    for (_frequency, points) in antenas_by_id {
+        let point_and_other_points = points.iter()
+            .enumerate()
+            .map(|(i, point)| (point, &points[(i + 1)..]));
+        for (point, other_points) in point_and_other_points {
+            for other in other_points {
+                // println!("{}: {} <-> {}", char::from_u32(*frequency as u32).unwrap(), point, other);
+                let diff = *other - *point;
+                antinodes.insert(*other + diff);
+                antinodes.insert(*point - diff);
+            }
+        }
+    }
+    antinodes.retain(|p| map.is_inside(*p));
+    // println!("{:?}", antinodes);
+
+    Ok(antinodes.len())
+}
