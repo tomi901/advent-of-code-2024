0189169ad078163a33361bd46c02819a7f9953c3
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2024-12-10 08:49:03 -0300

    Day 10 - Part 1

---
+++ day_10/src/lib.rs
@@ -0,0 +1,62 @@
+use std::{char, cmp::Reverse, collections::{BinaryHeap, HashSet}, str::FromStr};
+
+use xmas::{direction::DIRECTIONS, keyed_ord::KeyedOrd, map2d::ByteMap, point2d::Point2D};
+
+#[derive(Debug, Clone)]
+struct Breadcrumb {
+    point: Point2D,
+    value: u8,
+}
+
+impl Breadcrumb {
+    fn to_priority(self) -> Reverse<KeyedOrd<Breadcrumb, u8>> {
+        let key = self.value;
+        Reverse(KeyedOrd::new(self, key))
+    }
+}
+
+pub fn calculate_hiking_score(input: &str) -> u64 {
+    let map = ByteMap::from_str(input).unwrap();
+
+    map.iter_with_points()
+        .filter(|&(_, t)| t == &b'0')
+        .map(|(start, _)| calculate_hiking_score_from(&map, start))
+        .sum()
+}
+
+fn calculate_hiking_score_from(map: &ByteMap, start: Point2D) -> u64 {
+    // println!("Calculating paths from {}...", start);
+
+    let mut open_list = BinaryHeap::new();
+    open_list.push(Breadcrumb { point: start, value: b'0' }.to_priority());
+
+    let mut closed_list = HashSet::new();
+
+    let mut score = 0;
+    while let Some(cur_candidate) = open_list.pop() {
+        let cur_candidate = cur_candidate.0.value;
+
+        if closed_list.contains(&cur_candidate.point) {
+            continue;
+        }
+        closed_list.insert(cur_candidate.point);
+        // unsafe {
+        //     println!("Candidate {} @ {}", char::from_u32_unchecked(cur_candidate.value as u32), cur_candidate.point);
+        // }
+
+        if cur_candidate.value == b'9' {
+            score += 1;
+            continue;
+        }
+        
+        let next_value = cur_candidate.value + 1;
+        let new_candidates = DIRECTIONS.iter()
+            .map(|d| cur_candidate.point + d.as_point())
+            .flat_map(|p| map.get_tile(p).cloned().map(|t| (p, t)))
+            .filter(|&(_, t)| t == next_value)
+            .map(|(point, value)| Breadcrumb { point, value }.to_priority());
+        open_list.extend(new_candidates);
+    }
+
+    score
+}
