1aed86418cd59301a964300c5e7478043f23626c
Tomás Rinaldi <29787965+tomi901@users.noreply.github.com>
2024-12-17 23:47:32 -0300

    Day 17 - Part 2 - Tried to hardcode

--- day_17/src/lib.rs
+++ day_17/src/lib.rs
@@ -1,5 +1,5 @@
-use std::str::FromStr;
-
+use std::{str::FromStr, time::Duration};
+use genawaiter::{rc::gen, yield_};
 use regex_static::{once_cell::sync::Lazy, Regex, lazy_regex};
 
 const DIGIT_REGEX: Lazy<Regex> = lazy_regex!(r"\d+");
@@ -17,8 +17,52 @@ pub const INS_BDV: TinyByte = 6;
 pub const INS_CDV: TinyByte = 7;
 
 pub fn execute_all_instructions(input: &str) -> Vec<TinyByte> {
+    let (mut computer, program) = parse_input(input);
+    computer.execute(&program).collect()
+}
+
+pub fn calculated_required_a_value(input: &str) -> Register {
+    let (computer, program) = parse_input(input);
+    println!("Target output = {:?}", program);
+    // Testing from a hardcoded range
+    let mut best_candidate_count = 0;
+    'outer: for a in 0..64 {
+        let mut new_computer = Computer { register_a: a, ..computer.clone() };
+        let output: Vec<_> = new_computer.execute(&program).collect();
+        println!("{:#08b} = {:?}", a, output);
+
+        // std::thread::sleep(Duration::from_secs(1));
+        // if output == program {
+        //     return a;
+        // }
+
+        // if a % 10_000_000 == 0 {
+        //     println!("Testing: {}", a);
+        // }
+
+        // let mut output_iter = new_computer.execute(&program);
+        // let mut candidate_count = 0;
+        // for &expected in &program {
+        //     if !output_iter.next().is_some_and(|output| output == expected) {
+        //         if candidate_count > best_candidate_count {
+        //             println!("Best candidate ({}): {}/{}", a, candidate_count, program.len());
+        //             best_candidate_count = candidate_count;
+        //         }
+        //         continue 'outer;
+        //     }
+        //     candidate_count += 1;
+        // }
+
+        // if output_iter.next().is_none() {
+        //     return a;
+        // }
+    }
+    panic!("No solution found!");
+}
+
+fn parse_input(input: &str) -> (Computer, Vec<TinyByte>) {
     let (computer_s, program_s) = input.split_once("\n\n").unwrap();
-    let mut computer = Computer::from_str(computer_s).unwrap();
+    let computer = Computer::from_str(computer_s).unwrap();
     let program: Vec<TinyByte> = program_s.trim_start_matches("Program:")
         .trim()
         .split(',')
@@ -26,7 +70,7 @@ pub fn execute_all_instructions(input: &str) -> Vec<TinyByte> {
         .collect::<Result<_, _>>()
         .unwrap();
 
-    computer.execute(&program)
+    (computer, program)
 }
 
 pub fn output_result(result: &[TinyByte]) {
@@ -75,35 +119,36 @@ impl Computer {
         result
     }
 
-    pub fn execute(&mut self, instructions: &[TinyByte]) -> Vec<TinyByte> {
-        let mut program_counter: usize = 0;
-        let mut output = vec![];
-        while program_counter < instructions.len() {
-            let operator = instructions[program_counter];
-            let operand = instructions.get(program_counter + 1).cloned().unwrap_or(0);
-            match (operator, operand) {
-                (INS_ADV, operand) => self.register_a = self.dv_op(operand),
-                (INS_BXL, operand) => self.register_b ^= operand as u64,
-                (INS_BST, operand) => self.register_b = self.combo_value(operand) % 8,
-                (INS_JNZ, operand) => {
-                    if self.register_a != 0 {
-                        program_counter = operand as usize;
-                        continue;
+    pub fn execute<'a>(&'a mut self, instructions: &'a [TinyByte]) -> impl Iterator<Item = TinyByte> + 'a {
+        gen!({
+            let mut program_counter: usize = 0;
+            while program_counter < instructions.len() {
+                let operator = instructions[program_counter];
+                let operand = instructions.get(program_counter + 1).cloned().unwrap_or(0);
+                match (operator, operand) {
+                    (INS_ADV, operand) => self.register_a = self.dv_op(operand),
+                    (INS_BXL, operand) => self.register_b ^= operand as u64,
+                    (INS_BST, operand) => self.register_b = self.combo_value(operand) % 8,
+                    (INS_JNZ, operand) => {
+                        if self.register_a != 0 {
+                            program_counter = operand as usize;
+                            continue;
+                        }
                     }
+                    (INS_BXC, _) => self.register_b ^= self.register_c,
+                    (INS_OUT, operand) => {
+                        let val = self.combo_value(operand) as u8 % 8;
+                        yield_!(val);
+                        // println!("out: {:?}", output);
+                    },
+                    (INS_BDV, operand) => self.register_b = self.dv_op(operand),
+                    (INS_CDV, operand) => self.register_c = self.dv_op(operand),
+                    (ins, _) => unreachable!("Instruction {ins} not supported"),
                 }
-                (INS_BXC, _) => self.register_b ^= self.register_c,
-                (INS_OUT, operand) => {
-                    let val = self.combo_value(operand) as u8 % 8;
-                    output.push(val);
-                    // println!("out: {:?}", output);
-                },
-                (INS_BDV, operand) => self.register_b = self.dv_op(operand),
-                (INS_CDV, operand) => self.register_c = self.dv_op(operand),
-                (ins, _) => unreachable!("Instruction {ins} not supported"),
+                program_counter += 2;
             }
-            program_counter += 2;
-        }
-        output
+        })
+        .into_iter()
     }
 }
 
@@ -128,7 +173,7 @@ mod tests {
     fn example_1() {
         let mut computer = Computer::new(0, 0, 9);
 
-        computer.execute(&[2, 6]);
+        let _ = computer.execute(&[2, 6]).collect::<Vec<_>>();
 
         assert_eq!(computer.register_b, 1);
     }
@@ -137,7 +182,7 @@ mod tests {
     fn example_2() {
         let mut computer = Computer::new(10, 0, 0);
 
-        let output = computer.execute(&[5, 0, 1, 5, 4]);
+        let output: Vec<_> = computer.execute(&[5, 0, 1, 5, 4]).collect();
 
         assert_eq!(output, [0, 1, 2]);
     }
@@ -146,7 +191,7 @@ mod tests {
     fn example_3() {
         let mut computer = Computer::new(2024, 0, 0);
 
-        let output = computer.execute(&[0, 1, 5, 4, 3, 0]);
+        let output: Vec<_> = computer.execute(&[0, 1, 5, 4, 3, 0]).collect();
 
         assert_eq!(output, [4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0]);
     }
@@ -155,7 +200,7 @@ mod tests {
     fn example_4() {
         let mut computer = Computer::new(0, 29, 0);
 
-        computer.execute(&[1,7]);
+        let _ = computer.execute(&[1,7]).collect::<Vec<_>>();
 
         assert_eq!(computer.register_b, 26);
     }
@@ -164,7 +209,7 @@ mod tests {
     fn example_5() {
         let mut computer = Computer::new(0, 2024, 43690);
 
-        computer.execute(&[4,0]);
+        let _ = computer.execute(&[4,0]).collect::<Vec<_>>();
 
         assert_eq!(computer.register_b, 44354);
     }
--- day_17/src/main.rs
+++ day_17/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::{self, Context};
+use xmas::display_result;
 
 fn main() -> anyhow::Result<()> {
     part_1()?;
@@ -18,6 +19,9 @@ fn part_1() -> anyhow::Result<()> {
 
 fn part_2() -> anyhow::Result<()> {
     println!("Part 2:");
+    let input = std::fs::read_to_string("./input.txt").context("Error reading input file.")?;
 
+    let result = day_17::calculated_required_a_value(&input);
+    display_result(&result);
     Ok(())
 }
